9fb81aa84014c381e6c603bac8862081b4d4e886
Greg doud <gpdoud@gmail.com>
2017-09-14 11:46:51 -0400

    Showed LINQ Where, OrderByDescending, and Find

--- LINQLesson/Program.cs
+++ LINQLesson/Program.cs
@@ -3,27 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using EducationLibrary;
 
 namespace LINQLesson {
 	class Program {
-		static void Main(string[] args) {
-			StudentCollection students = StudentCollection.Select();
-			var someStudentData = students.Select(c => 
-				new {
-					FullName = $"{c.LastName}, {c.FirstName}",
-					GPA = c.GPA,
-					SAT = c.SAT
+
+		void RunExamplesFromChapter21() {
+
+		}
+		void RunStudentsWithMajorIdsLessThanFour() {
+			var students = StudentCollection.Select()
+				.Where(s => s.MajorId <= 4 && s.MajorId != -1)
+				.OrderByDescending(s => s.LastName);
+
+			var s10 = students.ToList().Find(s => s.Id == 10);
+			Debug.WriteLine($"{s10.FirstName} {s10.LastName} {s10.MajorId}");
+
+			//foreach (var s in students) {
+			//	Debug.WriteLine($"{s.FirstName} {s.LastName} {s.MajorId}");
+			//}
+		}
+		void RunJoiningMajorToStudent() {
+
+			List<Major> majors = new List<Major> {
+				new Major() { Id = 1, Description = "Audiology" },
+				new Major() { Id = 2, Description = "Biology" },
+				new Major() { Id = 3, Description = "Linguists" },
+				new Major() { Id = 4, Description = "CS" },
+				new Major() { Id = 5, Description = "ME" },
+				new Major() { Id = 6, Description = "IPS" }
+			};
+
+			var students = StudentCollection.Select();
+
+			var studentsAndMajors = from s in students
+									join m in majors
+									on s.MajorId equals m.Id
+									select new { s.FirstName, s.LastName, m.Description };
+
+			foreach(var s in studentsAndMajors) {
+				Debug.WriteLine($"{s.FirstName} {s.LastName} majoring in {s.Description}");
+			}
+		}
+		void Run() {
+			var students = StudentCollection.Select();
+
+			// to get students with GPA >= 3.5 and SAT > 1400 in GPA descending sequence WITHOUT LINQ
+			StudentCollection DeansListStudents = new StudentCollection();
+			foreach(var s in students) {
+				if(s.GPA >= 3.5 && s.SAT > 1400) {
+					DeansListStudents.Add(s);
 				}
-			);
-			foreach(var s in someStudentData) {
-				System.Diagnostics.Debug.WriteLine($"{s.FullName}, {s.GPA}, {s.SAT}");
 			}
-			System.Diagnostics.Debug.WriteLine("***************");
-			var somestudents = students.Where(c => c.Email == "a@b.c");
-			foreach (var s in somestudents) {
-				System.Diagnostics.Debug.WriteLine($"{s.FirstName + " " + s.LastName}, {s.GPA}, {s.SAT}");
+			// with LINQ
+			var TopStudents = students
+				.Where(stud => stud.GPA >= 3.5 && stud.SAT > 1400)
+				.OrderByDescending(s => s.GPA);
+
+			foreach(var student in TopStudents) {
+				Debug.WriteLine($"{student.FirstName} {student.LastName} GPA is {student.GPA} SAT is {student.SAT}");
 			}
+		}
+
+		static void Main(string[] args) {
+			new Program().RunExamplesFromChapter21();
 		}
 	}
 }
